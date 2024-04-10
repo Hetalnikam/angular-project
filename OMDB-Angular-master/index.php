cdbf2d6fcdc7bd66eea1f91a4e5991b135b43703
Hetalnikam <141377970+Hetalnikam@users.noreply.github.com>
2024-04-10 15:03:51 +0530

    Add files via upload

---
+++ OMDB-Angular-master/index.php
@@ -0,0 +1,20 @@
+
+<!DOCTYPE html>
+<html ng-app="omdb">
+<head>
+  <meta charset="utf-8">
+  <title>OMDB Angular App</title>
+  <link rel="stylesheet" type="text/css" href="./stylesheet/angStyle.css">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link href="http://fonts.googleapis.com/css?family=Corben:bold" rel="stylesheet" type="text/css">
+  <!-- links to jquery and angular from Google CDN -->
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+  <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.4.2/angular.min.js"></script>
+  <!--  links to angular and jquery files-->
+  <script src="./scripts/ang.js"></script>
+  <script src="./scripts/jquery.js"></script>
+</head>
+<body>
+  <?php include_once("home.html"); ?>
+</body>
+</html>
---
+++ OMDB-Angular-master/scripts/ang.js
@@ -0,0 +1,48 @@
+// console.log("Angular Loaded");
+
+// var app = angular.module('omdb', []);
+
+// app.controller('MovieController', function($scope, $http){
+//   this.movieSearch = '';
+//   this.formAction = function() {
+//     $http.get('https://www.omdbapi.com/?i=tt3896198&apikey=d0b1a674' + this.movieSearch + '&y=&r=json').
+//       then(function(response){
+//         $scope.movies = response.data.Search
+//     });
+//   };
+//   this.movieSelect = function(imdbID){
+//     $http.get('https://www.omdbapi.com/?i=tt3896198&apikey=d0b1a674' + imdbID + '&y=&plot=short&r=json').
+//       then(function(data){
+//         console.log(data.data);
+//         $scope.movie = data.data;
+//       });
+//   };
+
+// })
+console.log("Angular Loaded");
+
+var app = angular.module('omdb', []);
+
+app.controller('MovieController', function($scope, $http){
+  this.movieSearch = '';
+  this.apiKey = 'https://www.omdbapi.com/?i=tt3896198&apikey=d0b1a674'; // Replace 'YOUR_API_KEY' with your actual API key
+  
+  this.formAction = function() {
+    console.log("Form action called");
+    $http.get('https://www.omdbapi.com/?apikey=' + this.apiKey + '&s=' + this.movieSearch + '&y=&r=json').
+      then(function(response){
+        console.log("Search response:", response.data);
+        $scope.movieCtrl.movies = response.data.Search;
+    });
+  };
+
+  this.movieSelect = function(imdbID){
+    console.log("Movie selected:", imdbID);
+    $http.get('https://www.omdbapi.com/?apikey=' + this.apiKey + '&i=' + imdbID + '&y=&plot=short&r=json').
+      then(function(data){
+        console.log("Movie details:", data.data);
+        $scope.movieCtrl.movie = data.data;
+      });
+  };
+});
+
